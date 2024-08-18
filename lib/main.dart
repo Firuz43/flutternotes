994a49ec6899bd138c55f7bebf4d83ae50560813
Firuz Chamarov <firuz1199@mail.ru>
2024-08-18 18:40:57 +0400

    chagnes

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,40 @@ void main() {
     ),);
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController email;   // 1. we create email and password variables late means it
+  late final TextEditingController password; // assigns value later to the variables 
+
+
+  // 2. Then we will create initState that's given to us by statefulwidget
+  @override
+  void initState() {
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Registering'),
+      appBar: AppBar(title: const Text('Registering'), //kind of navigation widget
       backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: TextButton(onPressed: () {
-          
-        },child: const Text('Register'),),
+      body: Column(
+        children: [
+          TextField(),
+          TextField(),
+          TextButton(onPressed: () async {
+          },child: const Text('Register'),
+          ),
+        ],
       ),
     );
   }
