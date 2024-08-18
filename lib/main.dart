1608b8299068bd71cfb23296a003383c10391725
Firuz Chamarov <firuz1199@mail.ru>
2024-08-18 19:48:41 +0400

    creating authentication with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -21,15 +22,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  late final TextEditingController email;   // 1. we create email and password variables late means it
-  late final TextEditingController password; // assigns value later to the variables 
+  late final TextEditingController _email;   // 1. we create email and password variables late means it
+  late final TextEditingController _password; // assigns value later to the variables 
 
 
   // 2. Then we will create initState that's given to us by statefulwidget
   @override
   void initState() {
-
+    _email = TextEditingController();
+    _password = TextEditingController();
     super.initState();
+  }
+
+  // 3. Then we will create dispose function that is given to us by statefulwidget
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
   }
 
   @override
@@ -40,8 +50,18 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          TextField(),
-          TextField(),
+          TextField(
+            controller: _email, // We are taking them from our textcontroller
+            decoration: const InputDecoration( // This is placeholder
+              hintText: "Enter your email",
+            ),
+          ),
+          TextField(
+            controller: _password,
+            decoration: const InputDecoration(
+              hintText: "Enter your password",
+            ),
+          ),
           TextButton(onPressed: () async {
           },child: const Text('Register'),
           ),
