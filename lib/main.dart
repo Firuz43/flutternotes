8b9a6c84622b71f40d31578c73c6283186de8ba5
Firuz Chamarov <firuz1199@mail.ru>
2024-08-20 22:25:14 +0400

    separating login view and register view files

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,26 @@ class RegisterView extends StatefulWidget {
 }
 
 class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;   // 1. we create email and password variables late means it
+  late final TextEditingController _password; // assigns value later to the variables 
+
+
+  // 2. Then we will create initState that's given to us by statefulwidget
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  // 3. Then we will create dispose function that is given to us by statefulwidget //
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
