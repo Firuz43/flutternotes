ca84621355a6932230cc63ec0994105add405f6f
Firuz Chamarov <firuz1199@mail.ru>
2024-08-22 13:56:41 +0400

    changes

--- lib/views/login_views.dart
+++ lib/views/login_views.dart
@@ -67,7 +67,7 @@ class _LoginViewState extends State<LoginView> {
             ),
             TextButton(onPressed: () async {
               
-              final email = _email.text;  // 3. Getting emai and password details on button
+              final email = _email.text;  // 3. Getting emai and password details on button/
               final password = _password.text;
               final UserCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
                 email: email, 
