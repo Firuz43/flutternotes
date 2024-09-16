10c743d842a2fa10e2dbb8a8621b2cf886f04c80
Firuz Chamarov <firuz1199@mail.ru>
2024-09-16 14:37:48 +0400

    changes to our registerview design

--- lib/views/login_views.dart
+++ lib/views/login_views.dart
@@ -32,63 +32,69 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-  return Column(
-      children: [
-        TextField(
-          controller: _email, // We are taking them from our textcontroller//
-          enableSuggestions: false,
-          autocorrect: false,
-          keyboardType: TextInputType.emailAddress,
-          decoration: const InputDecoration( // This is placeholder
-            hintText: "Enter your email",
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Login view'),
+      backgroundColor: Colors.blue,
+    ),
+    body: Column(
+        children: [
+          TextField(
+            controller: _email, // We are taking them from our textcontroller//
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration( // This is placeholder
+              hintText: "Enter your email",
+            ),
           ),
-        ),
-        TextField(
-          controller: _password,
-          obscureText: true,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: const InputDecoration(
-            hintText: "Enter your password",
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: "Enter your password",
+            ),
           ),
-        ),
-        TextButton(onPressed: () async {
-          
-          final email = _email.text;  // 3. Getting emai and password details on button/
-          final password = _password.text;
-          try {
-            final UserCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-              email: email, 
-              password: password,  // 4. Authenticating it with firebase creating user
-            ); 
-            print(UserCredential);
-          } on FirebaseAuthException catch(e) {
-            if(e.code == 'invalid-credential') {
-              print('user not found');
-            }else {
-              print("Something else happened");
-              print(e.code);
+          TextButton(onPressed: () async {
+            
+            final email = _email.text;  // 3. Getting emai and password details on button/
+            final password = _password.text;
+            try {
+              final UserCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                email: email, 
+                password: password,  // 4. Authenticating it with firebase creating user
+              ); 
+              print(UserCredential);
+            } on FirebaseAuthException catch(e) {
+              if(e.code == 'invalid-credential') {
+                print('user not found');
+              }else {
+                print("Something else happened");
+                print(e.code);
+              }
             }
-          }
-          catch(e) {
-            print("Something bad happened");
-            print(e.runtimeType);
-            print(e);
-          }
-
-        },child: const Text('Login'),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/register/', 
-              (route) => false,
-            );
-          },
-          child: const Text('Not registered yet? register here'),
-        )
-      ],
-    );
+            catch(e) {
+              print("Something bad happened");
+              print(e.runtimeType);
+              print(e);
+            }
+    
+          },child: const Text('Login'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                '/register/', 
+                (route) => false,
+              );
+            },
+            child: const Text('Not registered yet? register here'),
+          )
+        ],
+      ),
+  );
   }
 }
 //
