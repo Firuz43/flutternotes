67a6d8212c2c9b44f5b55b01f53dbb075f8ee273
Firuz Chamarov <firuz1199@mail.ru>
2024-09-19 23:18:56 +0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _NotesViewState extends State<NotesView> {
         title: const Text('Main UI'),
         backgroundColor: Colors.blue, 
       ),
-      body: const Text('Hello World'), //
+      body: const Text('Hello World'), ////
     );
   }
 }
