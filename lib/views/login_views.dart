0ef344c1fb5a63c2ecf78a3bcdda1931ce8a413b
Firuz Chamarov <firuz1199@mail.ru>
2024-10-25 18:45:18 +0400

    view changes

--- lib/views/login_views.dart
+++ lib/views/login_views.dart
@@ -68,7 +68,7 @@ class _LoginViewState extends State<LoginView> {
               ); 
               print(UserCredential);
             } on FirebaseAuthException catch(e) {
-              if(e.code == 'invalid-credential') {//sss
+              if(e.code == 'invalid-credential') {//sssss
                 print('user not found');//
               }else {
                 print("Something else happened");
