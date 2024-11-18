5b11defc48992b6cfeb22119b21cd6aa7bffa823
Firuz Chamarov <firuz1199@mail.ru>
2024-11-18 21:40:45 +0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutternotes/firebase_options.dart';
-import 'package:flutternotes/views/calculator.dart';
 import 'package:flutternotes/views/login_views.dart';
 import 'package:flutternotes/views/register_view.dart';
 import 'package:flutternotes/views/verify_email_view.dart';
