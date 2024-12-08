74c443553673f1e763908e3cbccb10fc0bcfa774
Firuz Chamarov <firuz1199@mail.ru>
2024-12-08 11:21:10 +0400

    changes

--- lib/projects/to_do_list_app.dart
+++ lib/projects/to_do_list_app.dart
@@ -12,6 +12,19 @@ class _ToDoListAppState extends State<ToDoListApp> {
   //List to store tasks
   final List<Map<String, dynamic>> tasks = []; // each task is map with 'title' and 'isCompleted
 
+  // Controller to capture input
+  final TextEditingController taskController = TextEditingController();
+
+  // function to add new task
+  void addTask() {
+    if(taskController.text.isNotEmpty) {
+      setState(() {
+        tasks.add({'title': taskController.text, 'isCompleted': false});
+        taskController.clear(); // Clear the input field
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
