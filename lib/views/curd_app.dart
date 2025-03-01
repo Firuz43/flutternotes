954c894ce52c41e6e0f5c5e84b61b65e6ebcdf04
Firuz Chamarov <firuz1199@mail.ru>
2025-03-01 17:14:36 +0400

    changes

--- lib/views/curd_app.dart
+++ lib/views/curd_app.dart
@@ -10,6 +10,30 @@ class CurdApp extends StatefulWidget {
 class _CurdAppState extends State<CurdApp> {
   @override
   Widget build(BuildContext context) {
+    final List<String> items = []; //Temporary storage for items
+    final TextEditingController inputController = TextEditingController();
+
+    void addItem() {
+      if (inputController.text.isNotEmpty) {
+        setState(() {
+          items.add(inputController.text);
+          inputController.clear();
+        });
+      }
+    }
+
+    void editItem(int index, String newValue) {
+      setState(() {
+        items[index] = newValue;
+      });
+    }
+
+    void deleteItem(int index) {
+      setState(() {
+        items.removeAt(index);
+      });
+    }
+
     return const Placeholder();
   }
 }
