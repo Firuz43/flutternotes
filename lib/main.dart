de3659da7168bf6fffc034efe8b9d4ae8484bf97
Firuz Chamarov <firuz1199@mail.ru>
2025-03-03 05:12:01 +0400

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue, /////
-        primaryColor: Colors.blue, /////
+        primaryColor: Colors.blue, //////
       ),
       home: const HomePage(),
       routes: {
