fdb23d2b12d1b2dc04bf7cf0c546529fd8b5a087
Firuz Chamarov <firuz1199@mail.ru>
2025-04-13 11:40:54 +0400

    changes

--- lib/views/bmi_calc.dart
+++ lib/views/bmi_calc.dart
@@ -8,29 +8,25 @@ class BmiCalc extends StatefulWidget {
 }
 
 class _BmiCalcState extends State<BmiCalc> {
-
   final TextEditingController _wieghtController = TextEditingController();
   final TextEditingController _heightController = TextEditingController();
   String _result = "";
-
 
   void calculateBmi() {
     final String num1Text = _wieghtController.text;
     final String num2Text = _heightController.text;
 
-    if(num1Text.isNotEmpty && num2Text.isNotEmpty) {
+    if (num1Text.isNotEmpty && num2Text.isNotEmpty) {
       final double weigh = double.tryParse(num1Text) ?? 0;
       final double hight = double.tryParse(num2Text) ?? 0;
 
       setState(() {
         _result = "The BMI is: ${weigh / hight}";
       });
-    }else {
+    } else {
       _result = "Please enter valid numbers";
     }
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +35,6 @@ class _BmiCalcState extends State<BmiCalc> {
         title: Text("BMI calculator"),
         backgroundColor: Colors.blue,
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -48,32 +43,24 @@ class _BmiCalcState extends State<BmiCalc> {
               controller: _wieghtController,
               keyboardType: TextInputType.number,
               decoration: const InputDecoration(
-                labelText: "Enter weigh",
-                border: OutlineInputBorder()
-              ),
+                  labelText: "Enter weigh", border: OutlineInputBorder()),
             ),
-
             const SizedBox(height: 14),
-
             TextField(
               controller: _heightController,
               keyboardType: TextInputType.number,
               decoration: const InputDecoration(
-                labelText: "Enter height",
-                border: OutlineInputBorder()
-              ),
+                  labelText: "Enter height", border: OutlineInputBorder()),
             ),
             ElevatedButton(
-              onPressed: calculateBmi, 
+              onPressed: calculateBmi,
               child: Text("BMI"),
             ),
-
             const SizedBox(height: 16),
-
             Text(
               "The BMI Is: $_result",
               style: const TextStyle(fontSize: 18),
-              textAlign: TextAlign.center,
+              textAlign: TextAlign.center, //
             )
           ],
         ),
