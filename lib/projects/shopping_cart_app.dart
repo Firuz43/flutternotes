1b35b46f8a5bae9896e39fb2b537e6c0a8c1aba7
Firuz Chamarov <firuz1199@mail.ru>
2025-06-18 13:39:22 +0400

    changes

--- lib/projects/shopping_cart_app.dart
+++ lib/projects/shopping_cart_app.dart
@@ -9,7 +9,6 @@ class ShoppingCartApp extends StatefulWidget {
 }
 
 class _ShoppingCartAppState extends State<ShoppingCartApp> {
-
   //List of available products
   final List<Product> products = [
     Product("Apple", 1.2),
@@ -23,10 +22,12 @@ class _ShoppingCartAppState extends State<ShoppingCartApp> {
 
   void addToCart(Product product) {
     setState(() {
-      final index = cart.indexWhere((item) => item.product.name == product.name); // returns index if found or -1 if not found
-      if(index != -1) {
+      final index = cart.indexWhere((item) =>
+          item.product.name ==
+          product.name); // returns index if found or -1 if not found
+      if (index != -1) {
         cart[index].quantity++;
-      }else {
+      } else {
         cart.add(CartItem(product, 1));
       }
     });
@@ -39,65 +40,68 @@ class _ShoppingCartAppState extends State<ShoppingCartApp> {
   }
 
   double calculateTotal() {
-    return cart.fold(0.0, (sum, item) => sum + item.product.price * item.quantity);
+    return cart.fold(
+        0.0, (sum, item) => sum + item.product.price * item.quantity);
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Shopping Cart", style: TextStyle(color: Colors.white),),
+        title: Text(
+          "Shopping Cart",
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
       ),
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                final product = products[index];
-                return ListTile(
-                  title: Text(product.name),
-                  subtitle: Text("\$${product.price.toStringAsFixed(2)}"),
-                  trailing: IconButton(
-                    onPressed: () => addToCart(product), 
-                    icon: Icon(Icons.add_shopping_cart, color: Colors.green,)
-                  ),
-                );
-              }
-            )
-          ),
+              child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    final product = products[index];
+                    return ListTile(
+                      title: Text(product.name),
+                      subtitle: Text("\$${product.price.toStringAsFixed(2)}"),
+                      trailing: IconButton(
+                          onPressed: () => addToCart(product),
+                          icon: Icon(
+                            Icons.add_shopping_cart,
+                            color: Colors.green,
+                          )),
+                    );
+                  })),
           Divider(),
           Text(
             "Shopping Cart",
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: cart.length,
-              itemBuilder: (context, index) {
-                final cartItem = cart[index];
-                return ListTile(
-                  title: Text("${cartItem.product.name} (x${cartItem.quantity})"),
-                  subtitle: Text(
-                    "\$${(cartItem.product.price * cartItem.quantity).toStringAsFixed(2)}"
-                  ),
-                  trailing: IconButton(
-                    onPressed: () => removeFromCart(cartItem), 
-                    icon: Icon(Icons.delete, color: Colors.red,),
-                  ),
-                );
-              }
-            )
-          ),
-          Divider(),
+              child: ListView.builder(
+                  itemCount: cart.length,
+                  itemBuilder: (context, index) {
+                    final cartItem = cart[index];
+                    return ListTile(
+                      title: Text(
+                          "${cartItem.product.name} (x${cartItem.quantity})"),
+                      subtitle: Text(
+                          "\$${(cartItem.product.price * cartItem.quantity).toStringAsFixed(2)}"),
+                      trailing: IconButton(
+                        onPressed: () => removeFromCart(cartItem),
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                      ),
+                    );
+                  })),
+          Divider(), //
           Padding(
             padding: const EdgeInsets.all(8),
             child: Text(
               "Total: \$${calculateTotal().toStringAsFixed(2)}",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              
             ),
           )
         ],
